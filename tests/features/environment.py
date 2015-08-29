70b131344ca96e32d6f4bdd1d31b9891cec7de0d
Chang-Hung Liang <eliang.cs@gmail.com>
2015-08-29 16:24:24 +0800

    Cleanup and more documentation

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import getpass
 import os
-# import sys
+
 import pexpect
+
 import db_utils as dbutils
 import fixture_utils as fixutils
 
@@ -26,9 +28,9 @@ def before_all(context):
 
     url = urlparse(url)
     context.conf = {
-        'host': url.hostname,
-        'user': url.username,
-        'pass': url.password,
+        'host': url.hostname or 'localhost',
+        'user': url.username or getpass.getuser(),
+        'pass': url.password or '',
         'port': int(url.port or 5433),
         'dbname': url.path[1:]  # Ignore leading slash
     }
@@ -38,76 +40,6 @@ def before_all(context):
     context.cn = dbutils.create_cn(
         context.conf['host'], context.conf['pass'], context.conf['user'],
         context.conf['dbname'], context.conf['port'])
-
-
-# def before_all(context):
-#     """
-#     Set env parameters.
-#     """
-#     os.environ['LINES'] = "100"
-#     os.environ['COLUMNS'] = "100"
-#     os.environ['PAGER'] = 'cat'
-
-#     context.exit_sent = False
-
-#     # vi = '_'.join([str(x) for x in sys.version_info[:3]])
-#     # db_name = context.config.userdata.get('vcli_test', None)
-#     # db_name_full = '{0}_{1}'.format(db_name, vi)
-
-#     db_name_full = 'localdev'
-
-#     # Store get params from config.
-#     context.conf = {
-#         'host': context.config.userdata.get('vertica_test_host', 'localhost'),
-#         'user': context.config.userdata.get('vertica_test_user', 'dbadmin'),
-#         'pass': context.config.userdata.get('vertica_test_pass', 'pass'),
-#         'port': context.config.userdata.get('vertica_test_port', 5433),
-#         'dbname': db_name_full,
-#         'dbname_tmp': db_name_full + '_tmp',
-#     }
-
-#     # Store old env vars.
-#     context.env = {
-#         'VERTICA_DATABASE': os.environ.get('VERTICA_DATABASE', None),
-#         'VERTICA_USER': os.environ.get('VERTICA_USER', None),
-#         'VERTICA_HOST': os.environ.get('VERTICA_HOST', None),
-#         'VERTICA_PASSWORD': os.environ.get('VERTICA_PASSWORD', None),
-#     }
-
-#     # Set new env vars.
-#     os.environ['VERTICA_DATABASE'] = context.conf['dbname']
-#     os.environ['VERTICA_USER'] = context.conf['user']
-#     os.environ['VERTICA_HOST'] = context.conf['host']
-
-#     if context.conf['pass']:
-#         os.environ['VERTICA_PASSWORD'] = context.conf['pass']
-#     else:
-#         if 'VERTICA_PASSWORD' in os.environ:
-#             del os.environ['VERTICA_PASSWORD']
-#         if 'VERTICA_HOST' in os.environ:
-#             del os.environ['VERTICA_HOST']
-
-#     context.cn = dbutils.create_cn(
-#         context.conf['host'], context.conf['pass'], context.conf['user'],
-#         context.conf['dbname'], int(context.conf['port']))
-
-#     context.fixture_data = fixutils.read_fixture_files()
-
-
-# def after_all(context):
-#     """
-#     Unset env parameters.
-#     """
-#     dbutils.close_cn(context.cn)
-#     # dbutils.drop_db(context.conf['host'], context.conf['user'],
-#     #                 context.conf['pass'], context.conf['dbname'])
-
-#     # Restore env vars.
-#     for k, v in context.env.items():
-#         if k in os.environ and v is None:
-#             del os.environ[k]
-#         elif v:
-#             os.environ[k] = v
 
 
 def after_scenario(context, _):
