35be2f5c81fea42ba868b36f152d5e09e6cdda8f
Chang-Hung Liang <eliang.cs@gmail.com>
2015-09-07 22:20:24 +0800

    Cleanup

--- vcli/packages/vtablefmt.py
+++ vcli/packages/vtablefmt.py
@@ -2,14 +2,6 @@ from .tabulate import TableFormat, Line, DataRow
 
 
 # "Vertica-bar" seperated values
-vsv_aligned = TableFormat(lineabove=None,
-                          linebelowheader=Line('', '-', '+', ''),
-                          linebetweenrows=None,
-                          linebelow=None,
-                          headerrow=DataRow('', '|', ''),
-                          datarow=DataRow('', '|', ''),
-                          padding=1, with_header_hide=None)
-
 vsv_unaligned = TableFormat(lineabove=None,
                             linebelowheader=None,
                             linebetweenrows=None,
