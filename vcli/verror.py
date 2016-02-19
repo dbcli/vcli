0853d74c1ee15b28f308cba6c4145741c7937f50
Chang-Hung Liang <eliang.cs@gmail.com>
2016-02-19 16:52:47 +0800

    Format error message better in a long SQL

--- vcli/verror.py
+++ vcli/verror.py
@@ -25,6 +25,33 @@ def format_error(error):
     if match:
         sql = error.one_line_sql()
         position = int(match.group(1))
+
+        # Truncate the SQL query if its length > n (n must be odd)
+        n = 75
+        d = (n - 1) / 2
+        length = len(sql)
+        if length > n:
+            left = position - d
+            right = position + d
+            position = d
+            head = '...'
+            tail = '...'
+
+            if left < 0:
+                right -= left
+                position += left
+                left = 0
+                head = ''
+            elif right >= length:
+                offset = right - length + 1
+                left -= offset
+                position += offset
+                right = length - 1
+                tail = ''
+
+            sql = head + sql[left:right + 1] + tail
+            position += len(head)
+
         result += ('\n%s\n' % sql) + (' ' * (position - 1)) + '^'
 
     return result
