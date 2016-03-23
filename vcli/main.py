120a645221fecba50bb67ab08775ce0b39ba2b1a
Chang-Hung Liang <eliang.cs@gmail.com>
2016-03-23 18:03:08 +0800

    Fix #41: Need to run query twice to reconnect

--- vcli/main.py
+++ vcli/main.py
@@ -327,20 +327,14 @@ class VCli(object):
                 except NotImplementedError:
                     click.secho('Not Yet Implemented.', fg="yellow")
                 except errors.ConnectionError as e:
-                    reconnect = True
-                    if ('Connection is closed' in utf8tounicode(e.args[0])):
-                        reconnect = click.prompt('Connection reset. Reconnect (Y/n)',
-                                show_default=False, type=bool, default=True)
-                        if reconnect:
-                            try:
-                                vexecute.connect()
-                                click.secho('Reconnected!\nTry the command again.', fg='green')
-                            except errors.DatabaseError as e:
-                                click.secho(str(e), err=True, fg='red')
-                    else:
-                        logger.error("sql: %r, error: %r", document.text, e)
-                        logger.error("traceback: %r", traceback.format_exc())
-                        click.secho(str(e), err=True, fg='red')
+                    reconnect = click.prompt('Connection reset. Reconnect (Y/n)',
+                            show_default=False, type=bool, default=True)
+                    if reconnect:
+                        try:
+                            vexecute.connect()
+                            click.secho('Reconnected!\nTry the command again.', fg='green')
+                        except errors.DatabaseError as e:
+                            click.secho(str(e), err=True, fg='red')
                 except Exception as e:
                     logger.error("sql: %r, error: %r", document.text, e)
                     logger.error("traceback: %r", traceback.format_exc())
